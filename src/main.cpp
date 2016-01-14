763cf365aa231dd43550a2bc450718b8228245f7
YasueShuta <shuta.59074.orionsbelt@gmail.com>
2016-01-14 13:14:19 +0900

    add noise

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <random>
 #include "../eigen/Eigen/Core"
 #include "../gnuplot/Gnuplot.h"
 
@@ -29,15 +30,18 @@ int main(int argc, char* argv[]){
   static int SIZE_X = 10;
   static int SIZE_Y = 10;
   static int Image_Num = 5;
+  static int Image_Sample_Num = 10;
   
   static int Neutral_Num = 30;
   
-  static int Train_Num = 10000;
-  static double converge = 0.01;
+  static int Train_Num = 2000;
+  static double converge = 0.015;
 
   static double eta = 0.1;
+  static double trainNoise = 0.0;
+  static double NoiseRateMax = 0.25;
 
-
+  bool isConvert = true;
 
   /*  argv dealing  */
   {
@@ -55,6 +59,10 @@ int main(int argc, char* argv[]){
 	cout << "-y  set size_y" << endl;
 	cout << "-t  set train num" << endl;
 	cout << "-c  set converge threshold" << endl;
+	cout << "-i  set image num" << endl;
+	cout << "-s  set image sample num" << endl;
+	cout << "-v  no need to convert image" << endl;
+	cout << "-r  set max noise rate" << endl;
 	return 2;
       case 'm':
 	s = argv[++i];
@@ -79,6 +87,22 @@ int main(int argc, char* argv[]){
       case 'c':
 	s = argv[++i];
 	converge = stod(s);
+	break;      
+      case 's':
+	s = argv[++i];
+	Image_Sample_Num = stod(s);
+	break;
+      case 'i':
+	s = argv[++i];
+        Image_Num = stod(s);
+        break;
+      case 'v':
+	isConvert = false;
+	break;
+      case 'r':
+	s = argv[++i];
+	NoiseRateMax = stod(s);
+	break;
       default:
 	break;
       }
@@ -87,28 +111,42 @@ int main(int argc, char* argv[]){
   }
   
   /*  Create Image Data  */
-  vector<Mat> image_data = vector<Mat>(Image_Num);
+  vector<Mat> image_data = vector<Mat>(Image_Num*Image_Sample_Num);
   {  
     IplImage *src=0, *dst=0;
-    string str;
-    
+    string str_dir, str_file;
+    int index;
+
     for(int i = 0;i<Image_Num;i++){
-      str = "../image/im" + std::to_string(i+1) + ".png";
-      src = cvLoadImage(str.c_str(), CV_LOAD_IMAGE_GRAYSCALE);
-      
-      if(src == NULL) return -1;
-      
-      dst = cvCreateImage(cvSize(SIZE_X, SIZE_Y), src->depth, src->nChannels);
-      cvResize(src, dst, CV_INTER_NN);
-      
-      str = "../image_resize/sample" + to_string(i+1) + ".bmp";
-      image_data.at(i) = Mat(SIZE_Y, SIZE_X);
-      for(int h=0; h < dst->height; h++){
-	for(int w=0; w < dst->width; w++){
-	  image_data.at(i)(h, w) = (unsigned char)dst->imageData[h*dst->widthStep + w] / 256.0;
+      for(int j = 0;j<Image_Sample_Num;j++){
+	if(isConvert){
+	  str_dir = "../image/im" + to_string(i+1);
+	  str_file = "sam" + to_string(j+1);
+	  src = cvLoadImage((str_dir + "/" + str_file + ".png").c_str(), CV_LOAD_IMAGE_GRAYSCALE);
+	  
+	  if(src == NULL) return -1;
+	  
+	  dst = cvCreateImage(cvSize(SIZE_X, SIZE_Y), src->depth, src->nChannels);
+	  cvResize(src, dst, CV_INTER_NN);	
+	  
+	  str_dir = "../image_resize/im" + to_string(i+1);
+	  cvSaveImage((str_dir + "/" + str_file + ".bmp").c_str(), dst);
+	} else {
+	  str_dir = "../image_resize/im" + to_string(i+1);
+	  str_file = "sam" + to_string(j+1);
+	  dst = cvLoadImage((str_dir + "/" + str_file + ".bmp").c_str(), CV_LOAD_IMAGE_GRAYSCALE);
+	  
+	  if(dst == NULL) return -1;
+	}
+
+	index = j*Image_Num + i;
+	image_data.at(index) = Mat(SIZE_Y, SIZE_X);
+	for(int h=0; h < dst->height; h++){
+	  for(int w=0; w < dst->width; w++){
+	    image_data.at(index)(h, w) = (unsigned char)dst->imageData[h*dst->widthStep + w] / 256.0;
+	  }
 	}
       }
-      cvSaveImage(str.c_str(), dst);
     }
   }
   
@@ -133,23 +171,25 @@ int main(int argc, char* argv[]){
     
     ts = VectorXd::Zero(Image_Num);
   }
-
+  
   //Record
-  ofstream ofs("../record/record.txt");
-  ofs << "Assignment 1." << endl;
-  ofs << "No. Neutral Neuron: " << Neutral_Num << endl;
-  ofs << "Learning Rate:      " << eta << endl;
-  ofs << "Convergence       : " << converge << endl;
-  ofs << "Limit of Training : " << Train_Num << endl;
-  ofs << endl;
-  ofs << "Image Size        : " << SIZE_X << ", " << SIZE_Y << endl;
-  ofs << "Image Num         : " << Image_Num << endl;
-  ofs << endl;
+  ofstream rec_main("../record/rec_main.txt");
+  rec_main << "Assignment 2." << endl;
+  rec_main << "Add Noise 0 - " << (int)round(100*NoiseRateMax) << " %" << endl;
+  rec_main << "No. Neutral Neuron: " << Neutral_Num << endl;
+  rec_main << "Learning Rate:      " << eta << endl;
+  rec_main << "Convergence       : " << converge << endl;
+  rec_main << "Limit of Training : " << Train_Num << endl;
+  rec_main << endl;
+  rec_main << "Image Size        : " << SIZE_X << ", " << SIZE_Y << endl;
+  rec_main << "Image Num         : " << Image_Num << endl;
+  rec_main << "Sample Num        : " << Image_Sample_Num << endl;
+  rec_main << endl;
   ofstream rec, rec_train;
   GP *rec_fig = new GP(false, "wxt", "Record", "Times New Roman");
-
+  
   //Training
-  ofs << "[Training]" << endl;
+  rec_main << "[Training]" << endl;
   double E_RMS=1;
   VectorXd delta2 = x2;
   VectorXd delta3 = x3;
@@ -158,113 +198,219 @@ int main(int argc, char* argv[]){
 
     int count=0;
     int image_id;
-    while(count < Train_Num && E_RMS > converge){
-      E_RMS = 0;
-      for(image_id=0; image_id < Image_Num; image_id++){
-	//(1)initialize x1, ts
-	for(int i=0; i<x1.size(); i++){
-	  x1(i) = image_data.at(image_id)(i);
-	}
-	ts *= 0;
-	ts(image_id) = 1;
-	
-	//(2)front propagation
-	r1 = sigmoid(x1);
-	x2 = w12 * r1;
-	r2 = sigmoid(x2);
-	x3 = w23 * r2;
-	r3 = sigmoid(x3);
-	output = 0;
-	for(int i=0;i<r3.rows();i++){
-	  if(r3(output) < r3(i))
-	    output = i;
-	}
-	
-	//(3)back propagation
-	err = r3 - ts; 
-	delta3.array() = err.array() * d_sigmoid(x3).array();
-	w23 -= eta * delta3 * r2.transpose();
-	delta2 = (w23.transpose() * delta3).array() * d_sigmoid(x2).array();
-	w12 -= eta * delta2 * r1.transpose();
-	
-	//(4)calculate error evaluator
-	E_RMS += (err.transpose()*err)(0);
-      }
-      E_RMS = sqrt(E_RMS) / (double)(Image_Num*Image_Num);
-      cout << "(i: " << count << ") E_RMS: " << E_RMS << endl;
-      ofs << "(i: " << count << ")\tE_RMS: " << E_RMS << endl;
-      rec_train << count << " " << E_RMS << endl;
+    int sample_id;
+    int print_every=100;
+    double conv_d = 1.0;
+    double conv = 1.0;
+    bool isRecMode = false;
+    string rec_file;
+    char buf[30];
 
+    while(count < Train_Num && E_RMS > converge){
+      if(E_RMS <= conv) {
+	while(E_RMS <= conv){
+	  if(conv <= 2*conv_d){
+	    conv_d *= 0.1;
+	  }
+	  conv -= conv_d;
+	}
+	isRecMode = true;
+	cout << "[Rec Mode] " << E_RMS << "|" << conv+conv_d << endl;
+      }
+    
+      E_RMS = 0;
+      for(sample_id=0; sample_id < Image_Sample_Num; sample_id++){
+	for(image_id=0; image_id < Image_Num; image_id++){
+	  //(1)initialize x1, ts
+	  for(int i=0; i<x1.size(); i++){
+	    //Add Noise
+	    {
+	      random_device rd;
+	      mt19937 mt(rd());
+	      uniform_real_distribution<double> d(0.0,1.0);
+
+	      if(trainNoise && d(mt) <= trainNoise) {
+		x1(i) = d(mt);
+	      } else {
+		x1(i) = image_data.at(sample_id * Image_Num + image_id)(i);
+	      }
+	    }
+	  }
+	  ts *= 0;
+	  ts(image_id) = 1;
+	
+	  //(2)front propagation
+	  r1 = sigmoid(x1);
+	  x2 = w12 * r1;
+	  r2 = sigmoid(x2);
+	  x3 = w23 * r2;
+	  r3 = sigmoid(x3);
+	  output = 0;
+	  for(int i=0;i<r3.rows();i++){
+	    if(r3(output) < r3(i))
+	      output = i;
+	  }
+	  //(2')record output
+	  if(isRecMode){
+	    sprintf(buf, "../record/rec_test%d-%04d", image_id, (int)round(1000*(conv+conv_d)));
+	    rec_file = buf;
+	    rec.open(rec_file+".dat");
+	    for(int i=0;i<Image_Num;i++){
+	      rec << "Image" << i << " " << r3(i) << endl;
+	    }
+	    rec.close();
+
+	    rec_fig->hwrite("set xlabel");
+	    rec_fig->hwrite("set ylabel 'readout'");
+	    rec_fig->hwrite("set title 'recognize " + to_string(image_id) + " at " + to_string(conv+conv_d) + "'");
+	    rec_fig->hwrite("set style fill solid border lc rgb 'black'");
+	    rec_fig->plotFile(rec_file+".dat", "0:2:xtic(1)", "boxes", "lc rgb 'cyan'");
+	    rec_fig->save(rec_file+".png", "png");
+	  }
+	  
+	  //(3)back propagation
+	  err = r3 - ts; 
+	  delta3.array() = err.array() * d_sigmoid(x3).array();
+	  w23 -= eta * delta3 * r2.transpose();
+	  delta2 = (w23.transpose() * delta3).array() * d_sigmoid(x2).array();
+	  w12 -= eta * delta2 * r1.transpose();
+	  
+	  //(4)calculate error evaluator
+	  E_RMS += (err.transpose()*err)(0);
+	}
+	if(isRecMode) isRecMode = false;
+      }
+      E_RMS = sqrt(E_RMS) / (double)(Image_Num*Image_Num*Image_Sample_Num);
+
+      //(5)output E_RMS and count
+      if(count % print_every == 0){
+	cout << "(i: " << count << ") E_RMS: " << E_RMS << endl;
+	rec_train << count << " " << E_RMS << endl;
+      }
       count++;
     }
+   
+    //(6)record results
+    cout << "(i: " << count << ") E_RMS: " << E_RMS << "\t(Training Finished)" << endl;
+    rec_train << count << " " << E_RMS << endl;
     rec_train.close();
+
     rec_fig->hwrite("set xlabel 'count of training' font 'Times New Roman,20'");
     rec_fig->hwrite("set ylabel 'E_RMS' font 'Times New Roman,20'");
     rec_fig->plotFile("../record/rec_train.dat", "",  "lines", "lw 3 lc 'blue'");
     rec_fig->save("../record/rec_train.png", "png");
   }
 
-
   //Test
-  double result=0;
-  ofs << "[Test]" << endl;
+  std::vector<double> result(Image_Num, 0);
+  std::vector<double> totals;
+  rec_main << "[Test]" << endl;
   {
     string rec_file;
-    E_RMS = 0;
-    for(int image_id=0; image_id<Image_Num; image_id++) {
+    char buf[30];
+    double testNoise = 0.0;
+    for(testNoise = 0; testNoise < NoiseRateMax; testNoise += 0.05){
+      cout << "Noise Rate: " << 100*testNoise << " %" << endl;
+      rec_main << "Noise Rate: " << 100*testNoise << " %" << endl;
+      sprintf(buf,  "../record/rec_test%02d", (int)(100*testNoise));
+      rec_file = buf;
+      rec.open(rec_file + ".dat");
 
-	//(1)initialize x1, ts
-	for(int i=0; i<x1.size(); i++){
-	  x1(i) = image_data.at(image_id)(i);
+      E_RMS = 0;
+      for(int i=0;i<result.size();i++){
+	result.at(i) = 0;
+      }
+      for(int sample_id=0; sample_id<Image_Sample_Num; sample_id++){
+	for(int image_id=0; image_id<Image_Num; image_id++) {
+
+	  //(1)initialize x1, ts
+	  for(int i=0; i<x1.size(); i++){
+	    //Add Noise
+	    {
+	      random_device rd;
+	      mt19937 mt(rd());
+	      uniform_real_distribution<double> d(0.0,1.0);
+
+	      if(testNoise && d(mt) <= testNoise) {
+		x1(i) = d(mt);
+	      } else {
+		x1(i) = image_data.at(sample_id * Image_Num + image_id)(i);
+	      }
+	    }
+	  }
+	  ts *= 0;
+	  ts(image_id) = 1;
+	  
+	  //(2)front propagation
+	  r1 = sigmoid(x1);
+	  x2 = w12 * r1;
+	  r2 = sigmoid(x2);
+	  x3 = w23 * r2;
+	  r3 = sigmoid(x3);
+	  output = 0;
+	  
+	  //(4)judge output
+	  for(int i=0;i<r3.rows();i++){
+	    if(r3(output) < r3(i))
+	      output = i;
+	  }
+	  if(output == image_id) result.at(image_id)+=1;
+
+	  //(5)calculate error evaluator
+	  err = r3 - ts;
+	  E_RMS += (err.transpose()*err)(0);
 	}
-	ts *= 0;
-	ts(image_id) = 1;
-	
-	//(2)front propagation
-	r1 = sigmoid(x1);
-	x2 = w12 * r1;
-	r2 = sigmoid(x2);
-	x3 = w23 * r2;
-	r3 = sigmoid(x3);
-	output = 0;
+      }
 
-	cout << image_id << ": output: " << endl;
-	cout << r3.transpose() << endl;
-	ofs << image_id << ": Output: " << endl;
-	ofs << r3.transpose() << endl;
-	rec_file = "../record/rec_test" + to_string(image_id) + ".dat";
-	rec.open(rec_file.c_str());
-	for(int i=0;i<Image_Num;i++){
-	  rec << "Image" << i << " " << r3(i) << endl;
-	}
-	rec.close();
+    
+      //(6)record result
+      E_RMS = sqrt(E_RMS)/(double)(Image_Num*Image_Num);
+      cout << "E_RMS: " << E_RMS << endl;
+      rec_main << "E_RMS: " << E_RMS << endl;
 
-	rec_fig->hwrite("set xlabel");
-	rec_fig->hwrite("set ylabel 'readout'");
-	rec_fig->hwrite("set title 'recognize " + to_string(image_id) + "'");
-	rec_fig->hwrite("set style fill solid border lc rgb 'black'");
-	rec_fig->plotFile(rec_file.c_str(), "0:2:xtic(1)", "boxes", "lc rgb 'cyan'");
-	rec_file = "../record/rec_test" + to_string(image_id) + ".png";
-	rec_fig->save(rec_file, "png");
+      cout << "Correct :" << endl;
+      rec_main << "Correct :" << endl;
+      double p;
+      int total = 0;
+      for(int i=0; i<Image_Num; i++){
+	p = round(100*result.at(i)/(double)Image_Sample_Num);
+	cout << i << ":" << p << endl; 
+	rec_main << i << ":" << p << endl;      
+	rec << "Image" << i << " " << p << endl;
+	total += result.at(i+1);
+      }
 
-	for(int i=0;i<r3.rows();i++){
-	  if(r3(output) < r3(i))
-	    output = i;
-	}
-	if(output == image_id) result+=1;
-	ofs << "Judge: " << output << "(" << image_id << ")" << endl;
+      totals.push_back(round(100 * result.at(0)/(double)(Image_Sample_Num * Image_Num)));
 
-	//(3)calculate error evaluator
-	E_RMS += (err.transpose()*err)(0);
+      cout << "Total: " << totals.at(totals.size()-1) << " %" << endl;
+      rec_main << "Total: " << totals.at(totals.size()-1) << " %" << endl;
+      rec << endl << "Total " << totals.at(totals.size()-1) << endl;
+      rec.close();
+
+      //draw graph
+      rec_fig->hwrite("set xlabel");
+      rec_fig->hwrite("set ylabel 'Percent of Correct [%]'");
+      sprintf(buf, "set title 'Test: %02d%% Noise'", (int)round(100*testNoise));
+      rec_fig->hwrite(buf);
+      rec_fig->hwrite("set style fill solid border lc rgb 'cyan'");
+      rec_fig->plotFile(rec_file + ".dat", "boxes", "0:2:xtic(1)", "notitle", "");
+      rec_fig->save(rec_file + ".dat");   
     }
-    result /= (double)Image_Num;
-    E_RMS = sqrt(E_RMS)/(double)(Image_Num*Image_Num);
-    cout << "E_RMS: " << E_RMS << endl;
-    ofs << "E_RMS: " << E_RMS << endl;
-    cout << "Collect " << round(100*result) << " %" << endl;
-    ofs << "Collect " << round(100*result) << " %" << endl;
+    //Record Result
+    rec_file = "../record/rec_test-total";
+    rec.open(rec_file + ".dat");
+    for(int i=0;i<totals.size();i++){
+      rec << i*5 << " " << totals.at(i) << endl;
+    }
+    rec.close();
+
+    rec_fig->hwrite("set xlabel 'Noise Rate [%]'");
+    rec_fig->hwrite("set ylabel 'Percent of Correct [%}'");
+    rec_fig->hwrite("set title 'Noise Test'");
+    rec_fig->plotFile(rec_file + ".dat", "lines", "lw 3 lc 'red'", "notitle", "");
+    rec_fig->save(rec_file + ".png");
   }
-  ofs.close();
+  rec_main.close();
 
   return 0;
 }
